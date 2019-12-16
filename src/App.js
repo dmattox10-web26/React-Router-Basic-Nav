1771a1619fa2d781828c505ab2027605b02ea80e
Daniel Mattox <dmattox10@gmail.com>
2019-12-16 13:56:40 -0500

    Finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Route } from 'react-router-dom'
+
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
     <Navigation />
+    <Route exact path='/'>
+      <Home />
+    </Route>
+    <Route exact path='/about'>
+      <About />
+    </Route>
+    <Route exact path='/contact'>
+      <Contact />
+    </Route>
   </div>
 );
 
